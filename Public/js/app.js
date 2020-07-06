36e5891af72e0df30d94fc40d1b444b2bc86b968
Evan <esaito@nevada.unr.edu>
2020-07-06 14:09:39 -0700

    Incorrect HTML

--- Public/js/app.js
+++ Public/js/app.js
@@ -20,7 +20,7 @@ weatherForm.addEventListener('submit', (e) => {
     messageTwo.textContent = ''
 
     fetch('/weather?address=' + location).then((response) =>{
-        response.json().then((data) =>{
+        response.json().then((data) => {
             if (data.error) {
                 messageOne.textContent = data.error
             } else {
